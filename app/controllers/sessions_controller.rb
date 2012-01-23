ca4c62987512c7fb01ad160fc30b8e1b2d1c4098
Brett Hardin <hardin.brett@gmail.com>
2012-01-23 15:53:47 -0800

    Twitter Auth Now working

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,14 @@
 class SessionsController < ApplicationController
   def create
+    auth = request.env["omniauth.auth"]
+    user = User.find_by_provider_and_uid(auth["provider"], auth["uid"]) ||
+            User.create_with_omniauth(auth)
+    session[:user_id] = user.id
+    redirect_to '/', :notice => "Signed in!"
+  end
+
+  def error
+    #redirect_to '/', :notice => "Signed in!"
     raise request.env["omniauth.auth"].to_yaml
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ OAuthTest::Application.routes.draw do
 
   match '/' => 'pages#front_page'
   match "/auth/:provider/callback" => "sessions#create"
+  match "/auth/failure" => "sessions#error"
 end
