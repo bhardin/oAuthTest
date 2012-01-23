6c3f9758149cde4f7d86e949c8d74a7049f32e62
Brett Hardin <hardin.brett@gmail.com>
2012-01-23 15:31:41 -0800

    Twitter Redirect Working

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,8 @@
+class PagesController < ApplicationController
+
+  def front_page
+
+
+  end
+
+end
---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,3 @@
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :twitter, 'hmDDz6dvqKCT5VxkkzXNPg', 'TL6bMrGaavW7J3eWA0bsB5YF9uR69g0weCn982VJfmw'
+end
