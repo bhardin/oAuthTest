a37935130f4537347342c983dc5dcc35967bf2fd
Brett Hardin <hardin.brett@gmail.com>
2012-01-25 15:45:52 -0800

    Using Octokit to pull repo data

--- Gemfile
+++ Gemfile
@@ -7,6 +7,8 @@ gem 'omniauth'
 gem 'omniauth-twitter'
 gem 'omniauth-github'
 gem 'github-v3-api'
+gem 'octocat_herder'
+gem 'octokit'
 
 group :assets do
   gem 'sass-rails',   '~> 3.2.3'
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,14 @@
 class PagesController < ApplicationController
 
   def front_page
+    if current_user
+      @client = Octokit::Client.new(
+              :login => current_user.username,
+              :oauth_token => current_user.OAuthToken
+      )
 
-
+      @repos = @client.repos
+    end
   end
 
 end
